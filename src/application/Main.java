ed11c68866a552f1b4af94e80f995a9a1ae413bd
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2024-08-05 13:00:08 -0300

    0.1
    Criation Position class

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Position positon = new Position(3, 3);
+
+        System.out.println(positon);
+    }
+}
