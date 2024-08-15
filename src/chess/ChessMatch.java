5e3dfa74d9f65f2d29282d79bde30131e9d99f1c
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2024-08-15 09:37:53 -0300

    0.20

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,6 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.Pawn;
 import exceptions.ChessException;
 import chess.pieces.King;
@@ -192,7 +193,9 @@ public class ChessMatch {
 
     private void inicialSetup(){
         placeNewPiece('A', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('C', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('E', 1, new King(board, Color.WHITE));
+        placeNewPiece('F', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('H', 1, new Rook(board, Color.WHITE));
         placeNewPiece('A', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('B', 2, new Pawn(board, Color.WHITE));
@@ -204,7 +207,9 @@ public class ChessMatch {
         placeNewPiece('H', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('A', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('C', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('E', 8, new King(board, Color.BLACK));
+        placeNewPiece('F', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('H', 8, new Rook(board, Color.BLACK));
         placeNewPiece('A', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('B', 7, new Pawn(board, Color.BLACK));
--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -17,7 +17,7 @@ public class Bishop extends ChessPiece {
 
         Position p = new Position(0, 0);
 
-        // nw
+        // NorthWest
         p.setValues(position.getRow() - 1, position.getColumn() - 1);
         while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
@@ -27,7 +27,7 @@ public class Bishop extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
         }
 
-        // ne
+        // NorthEast
         p.setValues(position.getRow() - 1, position.getColumn() + 1);
         while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
@@ -37,7 +37,7 @@ public class Bishop extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
         }
 
-        // se
+        // SouhtEast
         p.setValues(position.getRow() + 1, position.getColumn() + 1);
         while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
@@ -47,7 +47,7 @@ public class Bishop extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
         }
 
-        // sw
+        // SouthWest
         p.setValues(position.getRow() + 1, position.getColumn() - 1);
         while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
@@ -59,7 +59,6 @@ public class Bishop extends ChessPiece {
 
         return mat;
     }
-
 
     @Override
     public String toString(){
